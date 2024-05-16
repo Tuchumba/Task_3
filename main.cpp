cbc4b09434eb10f177190d42453442cfa1ab32a6
Nikita Nedotsukov <nikita@bbnest.net>
2024-05-16 22:51:30 +0300

    add tests

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " input" << endl;
+        return -1;
+    }
     try {
-        Interpretator I("test.txt");
+        Interpretator I(argv[1]);
         I.interpretation();
         return 0;
     }
